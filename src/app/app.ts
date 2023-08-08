ceea97d4520eb7ce3f15dec9a82216a8c012ee5e
Thiago Leite <leite.tjs@gmail.com>
2023-08-08 20:16:55 -0300

    feat: build working

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,11 @@
 import fastify from 'fastify';
+import cors from '@fastify/cors';
 import { healthRoutes } from '../routes/health';
 import { githubRoutes } from '../routes/github';
 
 export const app = fastify();
+
+app.register(cors, {});
 
 app.register(healthRoutes, {
   prefix: '/api/health',
